950873cc320e7ee500bd002fac82d509c197e556
alisa <despero45@gmail.com>
2023-08-13 15:33:33 +1000

    Initial commit

---
+++ catalog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'Имя пользователя: {name}, Почта: {email}, Сообщение: {message}')
+    return render(request, 'catalog/contacts.html')
